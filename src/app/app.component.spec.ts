e7bf703da53c8b361f2f9de3d3a1bb140941fb62
Andras Lang <andras_lang@epam.com>
2022-11-15 22:28:04 +0100

    base components added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({})
+class DummyComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([{path: '**', component: DummyComponent}])],
       declarations: [
         AppComponent
       ],
@@ -16,16 +23,65 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontent-delevoper-test-v2'`, () => {
+  it(`should render the toolbar with two buttons`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontent-delevoper-test-v2');
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const toolbar = compiled.querySelector('mat-toolbar');
+    const btnPhotos = toolbar?.querySelector('#btn-photos');
+    const btnFavorites = toolbar?.querySelector('#btn-favorites');
+
+    expect(toolbar).toBeDefined();
+    expect(btnPhotos).toBeDefined();
+    expect(btnFavorites).toBeDefined();
   });
 
-  it('should render title', () => {
+  it(`should render a router-outlet`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontent-delevoper-test-v2 app is running!');
+    const toolbar = compiled.querySelector('router-outlet');
+
+    expect(toolbar).toBeDefined();
   });
+
+  it('should navigate to the default path when clicking on the "Photos" button and highlgiht the buttomn itself', fakeAsync(() => {
+    const router = TestBed.inject(Router);
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const btnPhotos = compiled.querySelector('#btn-photos') as HTMLElement;
+    const btnFavorites = compiled.querySelector('#btn-favorites') as HTMLElement; 
+
+    btnPhotos.click();
+    tick();
+
+    expect(router.url).toEqual('/');
+    expect(btnPhotos.classList.contains('active')).toBeTrue();
+    expect(btnFavorites.classList.contains('active')).toBeFalse();
+  }));
+
+  it('should navigate to the favorites path when clicking on the "Favorites" button', fakeAsync(() => {
+    const router = TestBed.inject(Router);
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const btnFavorites = compiled.querySelector('#btn-favorites') as HTMLElement; 
+    const btnPhotos = compiled.querySelector('#btn-photos') as HTMLElement;
+
+    btnFavorites.click();
+    tick();
+
+    expect(router.url).toEqual('/favorites');
+    expect(btnFavorites.classList.contains('active')).toBeTrue();
+    expect(btnPhotos.classList.contains('active')).toBeFalse();
+  }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app.routing.module';
+import { PhotosComponent } from './photos/photos.component';
+import { SinglePhotoComponent } from './single-photo/single-photo.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent,
+    SinglePhotoComponent,
+    FavoritesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
